4326fd200c0dc3fe1264a18d1f01966daf324cb6
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-06 16:22:59 -0600

    styled projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { About } from "./Sections/About"
 import { Skills } from "./Sections/Skills"
+import { Projects } from "./Sections/Projects"
 
 function App() {
   return (
     <>
       <About/>
       <Skills/>
+      <Projects/>
     </>
     
   )
