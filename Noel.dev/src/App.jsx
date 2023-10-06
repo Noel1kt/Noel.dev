186b8d93463deb7b2faad14a72c908b76efa5385
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-05 21:05:11 -0600

    section

--- Noel.dev/src/App.jsx
+++ Noel.dev/src/App.jsx
@@ -5,12 +5,17 @@ import './App.css'
 
 function App() {
   return (
-  <div>
-    <h3>Hola, mi nombre es</h3>
-    <h1>Noel Mazariegos.</h1>
-    <h2>Desarrollador Front-End</h2>
-    <p></p>
-  </div>
+    <section className='section-about'>
+      <div className='about-me'>
+        <h3>Hola, mi nombre es</h3>
+        <h1>Noel Mazariegos.</h1>
+        <h2>Desarrollador Front-End</h2>
+        <p>Actualmente me especializo en el desarrollo Front-end en el programa ONE de Oracle.
+          Tengo una gran pasión por los efectos de interfaz de usuario, las animaciones 
+          y la creación de experiencias intuitivas y dinámicas.</p>
+      </div>
+    </section>
+  
   )
 }
 
