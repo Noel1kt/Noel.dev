b05c2f8893b2df97b2b4bc5508b1c4815710c9f3
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-05 20:59:01 -0600

    titles

--- Noel.dev/src/App.jsx
+++ Noel.dev/src/App.jsx
@@ -4,7 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return <h1>Portafolio</h1>
+  return (
+  <div>
+    <h3>Hola, mi nombre es</h3>
+    <h1>Noel Mazariegos.</h1>
+    <h2>Desarrollador Front-End</h2>
+    <p></p>
+  </div>
+  )
 }
 
 export default App
