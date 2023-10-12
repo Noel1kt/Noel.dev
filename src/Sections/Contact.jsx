8181cf7c6d9acfe24d726b83aa5d57d00bc674a1
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-11 20:58:55 -0600

    contact style

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -2,20 +2,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSquareGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons" 
 import { faSquareInstagram, faSquareWhatsapp } from "@fortawesome/free-brands-svg-icons"
+import '../App.css'
 
 export const Contact = () => {
     return(
-        <>
+        <section className="section-contact">
             <h1>Contáctame</h1>
             <p>Aunque actualmente no estoy buscando nuevas oportunidades,
             pero puedes mandarme un mensaje en cualquiera de mis redes, ¡te contestaré lo antes posible!</p>
             <ul>
-                <li><a href=""><FontAwesomeIcon icon={faSquareGithub} /></a></li>
-                <li><a href=""><FontAwesomeIcon icon={faEnvelope} /></a></li>
-                <li><a href=""><FontAwesomeIcon icon={faLinkedin} /></a></li>
-                <li><a href=""><FontAwesomeIcon icon={faSquareInstagram} /></a></li>
-                <li><a href=""><FontAwesomeIcon icon={faSquareWhatsapp} /></a></li>
+                <li><a href=""><FontAwesomeIcon className="icon-list" size="3x"  icon={faSquareGithub} /></a></li>
+                <li><a href=""><FontAwesomeIcon className="icon-list" size="3x"  icon={faEnvelope} /></a></li>
+                <li><a href=""><FontAwesomeIcon className="icon-list" size="3x"  icon={faLinkedin} /></a></li>
+                <li><a href=""><FontAwesomeIcon className="icon-list" size="3x"  icon={faSquareInstagram} /></a></li>
+                <li><a href=""><FontAwesomeIcon className="icon-list" size="3x"  icon={faSquareWhatsapp} /></a></li>
             </ul>
-        </>
+        </section>
     )
 }
