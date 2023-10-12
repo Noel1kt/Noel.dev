8782159666bb6ba3467594840b52b73acb5f208b
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-11 20:13:35 -0600

    Contact

---
+++ src/Sections/Contact.jsx
@@ -0,0 +1,9 @@
+export const Contact = () => {
+    return(
+        <>
+            <h1>Contactame</h1>
+            <p>Aunque actualmente no estoy buscando nuevas oportunidades,
+            pero puedes mandarme un mensaje en cualquiera de mis redes, ¡te contestaré lo antes posible!</p>
+        </>
+    )
+}
