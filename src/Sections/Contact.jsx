64029eae6171b19631a6df235a824425d6c90939
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-11 22:00:29 -0600

    fixed icon style

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -11,11 +11,11 @@ export const Contact = () => {
             <p>Actualmente no estoy buscando nuevas oportunidades,
             pero puedes mandarme un mensaje en cualquiera de mis redes, ¡te contestaré lo antes posible!</p>
             <ul>
-                <li><a href="https://github.com/Noel1kt" target="_blank"><FontAwesomeIcon className="icon-list" size="3x" icon={faSquareGithub} /></a></li>
-                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-list" size="3x"  icon={faEnvelope} /></a></li>
-                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-list" size="3x"  icon={faLinkedin} /></a></li>
-                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-list" size="3x"  icon={faSquareInstagram} /></a></li>
-                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-list" size="3x"  icon={faSquareWhatsapp} /></a></li>
+                <li><a href="https://github.com/Noel1kt" target="_blank"><FontAwesomeIcon className="icon-contact" size="3x" icon={faSquareGithub} /></a></li>
+                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-contact" size="3x"  icon={faEnvelope} /></a></li>
+                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-contact" size="3x"  icon={faLinkedin} /></a></li>
+                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-contact" size="3x"  icon={faSquareInstagram} /></a></li>
+                <li><a href="" target="_blank"><FontAwesomeIcon className="icon-contact" size="3x"  icon={faSquareWhatsapp} /></a></li>
             </ul>
         </section>
     )
