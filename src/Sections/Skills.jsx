1fc560fc38bec26e3b077b87ee01ccff24f941b9
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-12 18:25:49 -0600

    animation skills

--- src/Sections/Skills.jsx
+++ src/Sections/Skills.jsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faReact,faSquareJs,faCss3Alt,faFigma,faNode,faPython,faGitAlt } from "@fortawesome/free-brands-svg-icons"
 import { faDatabase,faCloud } from "@fortawesome/free-solid-svg-icons"
+import React, { useEffect } from 'react';
+
 
 export const Skills = () => {
+   
     return(
         <section className="section-skills">
             <h1>Habilidades</h1>
@@ -10,21 +13,21 @@ export const Skills = () => {
             <div className="lists-skills">
                 <ul className="ul-skills">
                     <h2>Frontend</h2>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faReact} />ReactJS</li>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faSquareJs} />JavaScript</li>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faCss3Alt} />CSS</li>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faFigma} />Figma</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faReact} />ReactJS</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faSquareJs} />JavaScript</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faCss3Alt} />CSS</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faFigma} />Figma</li>
                 </ul>
                 <ul className="ul-skills">
                     <h2>Backend</h2>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faNode} />Node.js</li>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faDatabase} />MySQL</li>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faPython} />Python</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faNode} />Node.js</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faDatabase} />MySQL</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faPython} />Python</li>
                 </ul>
                 <ul className="ul-skills">
                     <h2>DevOps</h2>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faGitAlt} />Git</li>
-                    <li className="li-skills"><FontAwesomeIcon className="icon-list" size="2x" icon={faCloud} />Oracle Cloud</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faGitAlt} />Git</li>
+                    <li className="li-skills animated-element"><FontAwesomeIcon className="icon-list" size="2x" icon={faCloud} />Oracle Cloud</li>
                 </ul>
             </div>
             
