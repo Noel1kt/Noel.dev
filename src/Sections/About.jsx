7b339c2cfd0a34a1bda7ad4c551b63398922e8fa
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-12 20:16:19 -0600

    animation font img

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -12,8 +12,29 @@ export const About = () => {
             Tengo una gran pasión por los efectos de interfaz de usuario, las animaciones 
             y la creación de experiencias intuitivas y dinámicas.</p>
         </div>
-        <div className='profile-photo'>
-            <img className='profile-photo-img' src={profilePhoto} alt="" />
+        <div className='profile-photo' >
+            <div className="center">
+                <div className="ball"></div>
+                <div className="blubb-1"></div>
+                <div className="blubb-2"></div>
+                <div className="blubb-3"></div>
+                <div className="blubb-4"></div>
+                <div className="blubb-5"></div>
+                <div className="blubb-6"></div>
+                <div className="blubb-7"></div>
+                <div className="blubb-8"></div>
+                <div className="sparkle-1"></div>
+                <div className="sparkle-2"></div>
+                <div className="sparkle-3"></div>
+                <div className="sparkle-4"></div>
+                <div className="sparkle-5"></div>
+                <div className="sparkle-6"></div>
+                <div className="sparkle-7"></div>
+                <div className="sparkle-8"></div>
+                <div className="sparkle-9"></div>
+                <div className="sparkle-10"></div>
+                <img className='profile-photo-img' src={profilePhoto} alt="" />
+            </div>
         </div>
         </section>
     )
