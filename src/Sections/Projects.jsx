13615df0abc0a754b2b819a9f8f8cc6c0858a966
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-23 20:25:02 -0600

    target

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -24,7 +24,7 @@ export const Projects = () => {
                                 <p className="paragraph-project">Este proyecto surge como una iniciativa para la educación hambiental en Guatemala. 
                                     Se utilizó la librería React-Router-Dom para que la página sea una single-page application (SPA),
                                     además de simular una API con Vercel para obtener la información de las especies. <br/>
-                                    <a href="">Ver Proyecto <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="1x"/></a>
+                                    <a href="https://serpientes-gt.vercel.app/" target="_blank">Ver Proyecto <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="1x"/></a>
                                 </p>
                             </div>
                         </div>
@@ -38,7 +38,7 @@ export const Projects = () => {
                                     La creación de esta web first mobile surge como solución a la necesidad de hacer cuentas más rapidas
                                     y precisas de una producción avicola, cuenta con los datos de Hy-Line International con los que se consiguió
                                     adaptar las operaciones necesarias que realiza esta web. <br/>
-                                    <a href="">Ver Proyecto <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="1x"/></a>
+                                    <a href="https://avicola.vercel.app/" target="_blank">Ver Proyecto <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="1x"/></a>
                                 </p>
                             </div>
                         </div>
@@ -53,7 +53,7 @@ export const Projects = () => {
                                     Para la creación de esta web e-commerce se utilizaron conocimientos tanto de Frontend como 
                                     Backend, es un proyecto interesante y retador ya que combina las necesidades de tener una web llamativa y 
                                     funcional para los usuarios. <br/>
-                                    <a href="">Ver Proyecto <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="1x"/></a>
+                                    <a href="https://noel1kt.github.io/AluraGeek/" target="_blank">Ver Proyecto <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="1x"/></a>
                                 </p>
                             </div>
                         </div>
